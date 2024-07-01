e9fc2aba5b8c8a4caea4edae063fd81cba0f58fd
kimjaewoo2233 <79129475+kimjaewoo2233@users.noreply.github.com>
2024-07-01 23:49:06 +0900

    Modifying global.css file error

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,8 +1,23 @@
+import HeaderBox from "@/components/header-box";
 import React from "react";
 
 const Home = () => {
+    const loggedIn = { firstName: 'Adrian' };
+    
     return (
-        <div>HOME</div>
+        <section className="home">
+            <div className="home-content">
+                <header className="home-header">
+                    <HeaderBox
+                        type="greeting"
+                        title="Welcome"
+                        user={loggedIn?.firstName || 'Guest'}
+                        subtext="Access and manage your account and transactions efficiently."
+                    />
+                </header>
+
+            </div>
+        </section>
     )
 }
 
