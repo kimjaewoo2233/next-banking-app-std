458a4a27f6c65b74ed6383d114cb00731a9a5e72
kimjaewoo2233 <79129475+kimjaewoo2233@users.noreply.github.com>
2024-07-05 23:04:57 +0900

    다형성 컴포넌트 생성중

---
+++ src/components/recent-transactions.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+const RecentTransactions = ({
+    accounts,
+    transactions = [],
+    appwriteItemId,
+    page = 1,
+}: RecentTransactionsProps) => {
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default RecentTransactions;
